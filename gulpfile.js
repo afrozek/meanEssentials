b00d763c88104b09223ee2621973b3a1e0ba426a
Afroze Khan <afrozekhan@Afrozes-iMac.local>
2016-11-10 12:27:04 -0500

    seperated to builders, injectors, and watchers

--- gulpfile.js
+++ gulpfile.js
@@ -29,53 +29,111 @@ var nodemon = require('gulp-nodemon');
 				TASKS
 ======================================*/
 
-//tasks
-gulp.task('buildScripts', function(){
+//paths
+var jsPaths = ['src/app/**/*.module.js','src/app/**/*.js'];
+var sassPaths  = ['src/app/**/*.scss'];
 
-	//get modules files then the rest
-	 gulp.src(['src/app/**/*.module.js','src/app/**/*.js'])
+
+
+
+
+/*------------BUILDERS------------*/
+
+gulp.task('buildScripts', function() {
+//outputs main.js to src folder
+
+	 gulp.src(jsPaths)
 		.pipe(sourcemaps.init())
 		.pipe(concat('src/main.js'))
 		.pipe(uglify())
 		.pipe(sourcemaps.write('./'))
 		.pipe(gulp.dest('./'));
+	
+});
 
+gulp.task('buildStyles', function() {
+//outputs main.css to src folder
 
-
-	// sass
-	gulp.src('src/app/**/*.scss')
+	gulp.src(sassPaths)
     .pipe(sourcemaps.init())
     .pipe(sass().on('error', sass.logError))
     .pipe(concat('src/main.css'))
     .pipe(sourcemaps.write('./'))
     .pipe(gulp.dest('./'));
-
-
-	//inject
-	gulp.src('./src/index.html')
-	 	.pipe(inject(gulp.src(['./src/main.js','./src/main.css']),{relative: true}))	
-	 	.pipe(gulp.dest('./src'));
-
-})
-
-
-  
-
-
-
-
-gulp.task('watchSass', function() {
-	gulp.watch(['./public/assets/styles/*.scss'],['transpileSass'])
-})
-
-
-
-
-gulp.task('serve',['buildScripts'], function() {
 	
+});
+
+
+
+
+
+/*------------INJECTORS------------*/
+
+gulp.task('injectScripts', function() {
+//injects main.js into index.html
+
+	gulp.src('./src/index.html')
+ 	.pipe(inject(gulp.src(['./src/main.js','./src/main.css']),{relative: true}))	
+ 	.pipe(gulp.dest('./src'));
+	
+});
+
+
+gulp.task('injectStyles', function() {
+//injects main.css into index.html
+
+	gulp.src('./src/index.html')
+ 	.pipe(inject(gulp.src(['./src/main.js','./src/main.css']),{relative: true}))	
+ 	.pipe(gulp.dest('./src'));
+	
+});
+
+
+
+
+
+/*------------WATCHERS------------*/
+
+gulp.task('watchScripts', function() {
+	gulp.watch(jsPaths,['buildScripts','injectScripts'])
+});
+
+gulp.task('watchStyles', function() {
+	gulp.watch(sassPaths,['buildStyles','injectStyles'])
+});
+
+
+
+
+/*------------DOERS------------*/
+
+gulp.task('serve',['buildScripts','buildStyles','injectScripts','injectStyles','watchScripts','watchStyles'], function() {
+// starts server, runs tasks
+
 	nodemon({
 		script: 'index.js'
 	})
 
 
-})
+});
+
+
+
+
+/*======================================
+			  DIST BUILD
+======================================*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
