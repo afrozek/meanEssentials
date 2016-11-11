d27ee24d331261ac970ecd36800417892364a11f
Afroze Khan <afrozekhan@Afrozes-iMac.local>
2016-11-11 18:48:40 -0500

    added prod mode and dev mode in indexjs file

--- index.js
+++ index.js
@@ -2,14 +2,32 @@ var express = require('express');
 var app = express();
 var path = require('path');
 
-// app.use(express.static(__dirname + '/src'));
-// app.use(express.static(__dirname + '/bower_components'));
-// app.use(express.static(__dirname + '/app'));
-app.use(express.static(__dirname + '/dist'));
 
-app.get('*',function(req,res){
-	res.sendFile(path.join(__dirname + '/dist/index.html'));
-});
+
+
+var env = "development";
+var env = "production";
+
+
+if(env == "production"){
+	console.log("production mode");
+	app.use(express.static(__dirname + '/dist'));
+
+	app.get('*',function(req,res){
+		res.sendFile(path.join(__dirname + '/dist/index.html'));
+	});
+}
+else{
+	console.log("development mode");
+	app.use(express.static(__dirname + '/src'));
+	app.use(express.static(__dirname + '/bower_components'));
+
+	app.get('*',function(req,res){
+		res.sendFile(path.join(__dirname + '/src/index.html'));
+	});
+}
+
+
 
 // app.use(express.static(__dirname));
 
--- src/main.js
+++ src/main.js
@@ -1,2 +1,210 @@
-!function(){"use strict";angular.module("app",["ui.router","landing"])}(),function(){"use strict";angular.module("landing",[])}(),angular.module("templates").run(["$templateCache",function(n){n.put("app.view.html","<h1>appView</h1>\n<h2>controllerTest: {{appCtrl.controllerName}}</h2>\n<div ui-view></div>"),n.put("appComponents/landing/views/landing.view.html",'<div id="landing" class="container" >\n\t<div class="page-header centerText">\n\t  <h1>landing</h1>\n\t</div>\n</div>\n')}]),function(){"use strict";function n(){var n=this;n.controllerName="appController"}angular.module("app").controller("appController",n),n.$inject=[]}(),function(){"use strict";function n(n,t,e){n.state("app",{abstract:!0,templateUrl:"app/app.view.html",controller:"appController",controllerAs:"appCtrl",data:{requireLogin:!1}}).state("app.landing",{url:"/",templateUrl:"app/appComponents/landing/views/landing.view.html",controller:"landingController",controllerAs:"landingCtrl",data:{requireLogin:!1}}),t.otherwise("/"),e.html5Mode(!0)}angular.module("app").config(n),n.$inject=["$stateProvider","$urlRouterProvider","$locationProvider"]}(),function(){"use strict";function n(){function n(){}function t(){}function e(){}var r=this;r.gotoSession=n,r.refresh=t,r.search=e,r.sessions=[],r.title="landing"}angular.module("landing").controller("landingCtrl",n),n.$inject=[]}(),function(){function n(){return{restrict:"E",templateUrl:"",replace:!0}}angular.module("landing").directive("landingDir",n)}(),function(){"use strict";function n(){function n(){}function t(){console.log("landingService")}function e(){}var r={error:n,info:t,success:e};return r}angular.module("landing").factory("landingService",n),n.$inject=[]}();
+(function() {
+  'use strict';
+
+  angular
+    .module('app', [
+    	'ui.router',
+    	'landing'
+    ]);
+})();
+
+(function(){
+	'use strict'
+
+angular
+	.module('landing', [
+	  
+	]);
+
+})();
+
+angular.module('app').run(['$templateCache', function($templateCache) {$templateCache.put('app/app.view.html','<h1>appView</h1>\n<h2>controllerTest: {{appCtrl.controllerName}}</h2>\n<div ui-view></div>');
+$templateCache.put('app/appComponents/landing/views/landing.view.html','<div id="landing" class="container" >\n\t<div class="page-header centerText">\n\t  <h1>landing</h1>\n\t</div>\n</div>\n');}]);
+
+(function() {
+  'use strict';
+
+  angular
+    .module('app')
+    .controller('appController', appController);
+
+  appController.$inject = [];
+
+  /** @ngInject */
+  function appController() {
+    var vm = this;
+    vm.controllerName = "appController";
+
+
+  
+
+  }//end appController
+   
+})();
+
+
+
+
+
+  
+  /////////////
+
+
+(function() {
+  'use strict';
+
+
+
+
+  angular
+    .module('app')
+    .config(routerConfig);
+
+    routerConfig.$inject = ['$stateProvider','$urlRouterProvider', '$locationProvider'];
+
+  /** @ngInject */
+   function routerConfig($stateProvider, $urlRouterProvider, $locationProvider) {
+    $stateProvider
+  
+
+      .state('app', {
+        abstract: true,
+        templateUrl: 'app/app.view.html',
+        controller: 'appController',
+        controllerAs: 'appCtrl',
+        data: {
+          requireLogin: false
+        }
+      })
+
+      .state('app.landing', {
+        url: '/',
+        templateUrl: 'app/appComponents/landing/views/landing.view.html',
+        controller: 'landingController',
+        controllerAs: 'landingCtrl',
+        data: {
+          requireLogin: false
+        }
+      })
+
+      .state('test', {
+        url: '/test',
+        template: '<h1>HI!</h1>',
+
+      })
+
+
+
+    $urlRouterProvider.otherwise('/');
+    $locationProvider.html5Mode(true);
+
+  }//end routerConfig function
+
+
+
+
+})();
+
+
+(function() {
+	'use strict'
+
+	angular
+		.module('landing')
+		.controller('landingController', landingController)
+
+	landingController.$inject = []
+
+	function landingController() {
+
+	    var vm = this;
+
+	    vm.gotoSession = gotoSession;
+	    vm.refresh = refresh;
+	    vm.search = search;
+	    vm.sessions = [];
+	    vm.title = 'landing';
+
+	    ////////////
+
+	    function gotoSession() {
+	      /* */
+	    }
+
+	    function refresh() {
+	      /* */
+	    }
+
+	    function search() {
+	      /* */
+	    }
+	}
+
+
+//end IIFE
+})();
+
+
+
+
+(function(){
+angular
+    .module('landing')
+    .directive('landingDir', landingDir);
+
+function landingDir() {
+	return{
+		restrict: 'E',
+		templateUrl: '',
+		replace: true
+		// scope: {}
+	}
+}
+
+//end IIFE
+})();
+
+(function(){
+	'use strict'
+
+	angular
+    	.module('landing')
+    	.factory('landingService', landingService);
+
+    landingService.$inject = []
+
+    function landingService() {
+    	var service = {
+
+    		error: error,
+    		info: info,
+    		success: success
+
+    	};
+
+    	return service;
+
+    	////////////
+
+    	function error() {
+	      /* */
+	    }
+
+	    function info() {
+	      /* */
+          console.log("landingService");
+	    }
+
+	    function success() {
+	      /* */
+	    }
+
+
+    }
+
+	
+// end IIFE
+})();
+
+
 //# sourceMappingURL=main.js.map
